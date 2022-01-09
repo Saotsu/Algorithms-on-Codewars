8e6a117e36f80cd6357366c653b4935acc57a208
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-09 19:22:47 -0300

    Update Where my anagrams at?.py

--- Where my anagrams at?.py
+++ Where my anagrams at?.py
@@ -1,5 +1,4 @@
 #https://www.codewars.com/kata/523a86aa4230ebb5420001e1
 
 def anagrams(word, words):
-    s = [w for w in words if sorted(w) == sorted(word)]
-    return s if s else []
+    return [w for w in words if sorted(w) == sorted(word)]
