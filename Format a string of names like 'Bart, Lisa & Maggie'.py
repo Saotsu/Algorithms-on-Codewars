cc79d531e17c5667434bc48dd7103110d9bd363a
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-09 18:47:04 -0300

    Update Format a string of names like 'Bart, Lisa & Maggie'.py

--- Format a string of names like 'Bart, Lisa & Maggie'.py
+++ Format a string of names like 'Bart, Lisa & Maggie'.py
@@ -5,9 +5,5 @@ def namelist(names):
         return ""
     elif len(names) == 1:
         return names[0]['name']
-    elif len(names) == 2:
-        return " & ".join(n['name'] for n in names)
-    else:
-        s = ", ".join(n['name'] for n in names)
-        s = s.replace(f", {names[-1]['name']}", f" & {names[-1]['name']}")
-        return s
+    s = ", ".join(n['name'] for n in names)
+    return s.replace(f", {names[-1]['name']}", f" & {names[-1]['name']}")
