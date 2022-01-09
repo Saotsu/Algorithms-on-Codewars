9a12a07d3ad930d308e47181d52894f54e155227
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-09 20:54:33 -0300

    Update Roman Numerals Helper.py

--- Roman Numerals Helper.py
+++ Roman Numerals Helper.py
@@ -1,5 +1,6 @@
 #https://www.codewars.com/kata/51b66044bce5799a7f000003
 
+#This code contains interative and recursive solution for this problem
 #Recursive solution
 
 class RomanNumerals:
@@ -32,6 +33,29 @@ class RomanNumerals:
                 g += r[0][1]
             return g + RomanNumerals.from_roman(roman_num, r[1:])
         return 0
+    
+    #Iterative solution
+    
+    class RomanNumerals:
+    def to_roman(val):
+        r = [('M', 1000), ('CM', 900), ('D', 500), ('CD', 400), ('C', 100), ('XC', 90), 
+        ('L', 50), ('XL', 40), ('X', 10), ('IX', 9), ('V', 5), ('IV', 4), ('I', 1)]
+        s = ''
+        for i in r:
+            while val >= i[1]:
+                val -= i[1]
+                s += i[0]
+        return s
+    
+    def from_roman(roman_num):
+        r = [('CM', 900), ('CD', 400), ('XC', 90), ('XL', 40), ('IX', 9), ('IV', 4), 
+         ('M', 1000), ('D', 500),('C', 100),('L', 50),('X', 10),('V', 5),('I', 1)]
+        s = 0
+        for i in r:
+            while i[0] in roman_num:
+                 roman_num = roman_num.replace(i[0], '', 1)
+                 s += i[1]
+        return s
       
       
       
