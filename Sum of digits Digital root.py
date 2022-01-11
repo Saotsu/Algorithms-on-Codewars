f46c221137e5908e897c323cbdf61453f3bac856
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-10 21:06:55 -0300

    Update Sum of digits Digital root.py

--- Sum of digits Digital root.py
+++ Sum of digits Digital root.py
@@ -1,6 +1,10 @@
 #https://www.codewars.com/kata/541c8630095125aba6000c00
 
+#Iterative solution
 def digital_root(n):
     while len(str(n)) >= 2:
         n = sum(int(b) for b in str(n))
     return n
+
+#Recursive solution
+digital_root = lambda n: n if len(str(n)) == 1 else digital_root(sum(int(b) for b in str(n)))
