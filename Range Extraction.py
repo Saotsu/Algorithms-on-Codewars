0255abefc92a6dcbb6f8cdc6cd255ceaa9c5a54f
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-13 03:03:24 -0300

    Create Range Extraction.py

---
+++ Range Extraction.py
@@ -0,0 +1,23 @@
+#https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
+
+def solution(args):
+    c = []
+    d = 0
+    for i, e in enumerate(args):
+        if i > 0:
+            if d == 0:
+                if e - 1 == args[i-1]:
+                    d = 1
+                    c = [args[i-1]] + [e]    
+                elif e + 1 == args[i-1]:
+                    d = -1
+                    c = [args[i-1]] + [e]
+                else:
+                    return f'{args[i-1]},' + solution(args[i:])
+            elif e == c[-1] + d:
+                c.append(e)
+            elif len(c) >= 3:
+                return f'{c[0]}-{c[-1]},' + solution(args[i:])
+            else:
+                return (",".join(str(l) for l in c)) + "," + solution(args[i:])
+    return f'{c[0]}-{c[-1]}' if len(c) >= 3 else f'{e}' if not c else (",".join(str(l) for l in c))
