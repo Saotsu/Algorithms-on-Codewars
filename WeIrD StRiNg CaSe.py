bfe5d26a4d7eafc419196ac7c8123c9ddee8bed3
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-14 04:59:06 -0300

    Update WeIrD StRiNg CaSe.py

--- WeIrD StRiNg CaSe.py
+++ WeIrD StRiNg CaSe.py
@@ -1,1 +1,10 @@
 #https://www.codewars.com/kata/52b757663a95b11b3d00062d
+
+def to_weird_case(string):
+    if isinstance(string, str):
+        string = string.split()
+    for w in string:
+        w = "".join(w[i].upper() if i % 2 == 0 else w[i].lower() for i in range(len(w)))
+        if len(string) > 1:
+            return w + " " + to_weird_case(string[1:])
+        return w
