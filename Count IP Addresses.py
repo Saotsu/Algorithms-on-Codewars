563abaac4bbccaab274bac6fe44a1d6dd5366fe4
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-14 01:44:53 -0300

    Update Count IP Addresses.py

--- Count IP Addresses.py
+++ Count IP Addresses.py
@@ -1,4 +1,9 @@
 #https://www.codewars.com/kata/526989a41034285187000de4
 
 def ips_between(start, end):
-    return (sum((int(number[1]) * 256 ** index) - (int(number[0]) * 256 ** index) for index, number in enumerate(list(zip(start.split(".")[::-1], end.split(".")[::-1])))))
+    ips = list(zip(start.split(".")[::-1], end.split(".")[::-1]))
+    result = 0
+    for index, number in enumerate(ips):
+        result += (int(number[1]) * 256 ** index) - (int(number[0]) * 256 ** index)
+    return result
+
