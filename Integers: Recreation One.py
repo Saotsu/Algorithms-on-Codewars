606f04472870590b53783b945715ab9e17a940cd
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-14 02:35:56 -0300

    Update Integers: Recreation One.py

--- Integers: Recreation One.py
+++ Integers: Recreation One.py
@@ -1,1 +1,16 @@
 #https://www.codewars.com/kata/55aa075506463dac6600010d
+
+def list_squared(m, n):
+    def squared(number):
+        s = []
+        for n in range(1, int(number**0.5)+1):
+            if number % n == 0:
+                s.extend([n**2, int(number / n)**2])
+        return sum(set(s))
+    
+    results = []
+    for x in range(m, n):
+        s = squared(x)
+        if (s**0.5) % 1 == 0:
+            results.append([x, s])
+    return results
