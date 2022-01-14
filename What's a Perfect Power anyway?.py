b53f0370ecc2e7b59895ae7fc9bb0ad1a2b06e3c
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-14 06:55:32 -0300

    Update What's a Perfect Power anyway?.py

--- What's a Perfect Power anyway?.py
+++ What's a Perfect Power anyway?.py
@@ -1,2 +1,9 @@
 #https://www.codewars.com/kata/54d4c8b08776e4ad92000835
 
+from math import log2
+
+def isPP(n):
+    for p in range(2, int(log2(n))+1):
+        if round(n**(1/p))**p == n:
+            return [round(n**(1/p)), p]
+    return None
