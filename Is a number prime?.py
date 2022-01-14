ed2accac8e0fdfd121b7350c1436fa972d102096
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-14 04:21:51 -0300

    Update Is a number prime?.py

--- Is a number prime?.py
+++ Is a number prime?.py
@@ -1,1 +1,10 @@
 #https://www.codewars.com/kata/5262119038c0985a5b00029f
+
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for n in range(2, int(num**0.5)+1):
+        if num % n == 0:
+            return False
+    return True
