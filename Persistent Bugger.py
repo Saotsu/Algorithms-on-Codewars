882180ef6a2e27c4a70b6aa4db69bc485ccad2ad
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-14 03:19:18 -0300

    Update Persistent Bugger.py

--- Persistent Bugger.py
+++ Persistent Bugger.py
@@ -1,1 +1,9 @@
 #https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+
+def persistence(n):
+    if n < 10:
+        return 0
+    r = 1
+    for n in str(n):
+        r *= int(n)
+    return 1 + persistence(r)
