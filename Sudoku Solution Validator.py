4bf79672f1035623f1d472292668f57a27f6a097
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-21 06:35:02 -0300

    Update Sudoku Solution Validator.py

--- Sudoku Solution Validator.py
+++ Sudoku Solution Validator.py
@@ -1,10 +1,8 @@
 #https://www.codewars.com/kata/529bf0e9bdf7657179000008
 
 def valid_solution(board):
-    for g in range(3):
-       for h in range(3):
-            y = g * 3
-            x = h * 3
+    for y in range(0, 7, 3):
+       for x in range(0, 7, 3):
             if len(set(board[y][x:x+3]+board[y+1][x:x+3]+board[y+2][x:x+3])) != 9:
                 return False
                 
