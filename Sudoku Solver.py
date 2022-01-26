81c24cf049a2e09af9c13ed6f35d5158fdb37945
Saotsu <96318037+Saotsu@users.noreply.github.com>
2022-01-26 19:33:04 -0300

    Update Sudoku Solver.py

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -14,17 +14,24 @@ def check(x, y, v, sudoku):
         return False
 
     return True
-    
+
 def sudoku(puzzle):
+    m, n = 0, 0
     for y in range(9):
-        for x in range(9):
-            if puzzle[y][x] == 0:
-                for v in range(1, 10):
-                    if check(x, y, v, puzzle):
-                        puzzle[y][x] = v
-                        sudoku(puzzle)
-                        if 0 not in [puzzle[y][x] for y in range(9) for x in range(9)]:
-                            return puzzle
-                        puzzle[y][x] = 0
-                else:
-                    return None
+         for x in range(9):
+             if puzzle[y][x] == 0:
+                 m, n = x, y
+    def solve(puzzle, m, n):
+        for y in range(9):
+            for x in range(9):
+                if puzzle[y][x] == 0:
+                    for v in range(1, 10):
+                        if check(x, y, v, puzzle):
+                            puzzle[y][x] = v
+                            solve(puzzle, m, n)
+                            if puzzle[n][m] != 0:
+                                return puzzle
+                            puzzle[y][x] = 0
+                    else:
+                        return None
+    return solve(puzzle, m, n)
